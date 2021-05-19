b017c2c14b870c9ca7efcf344e263e3095b09fd2
darkninja1 <49328567+darkninja1@users.noreply.github.com>
2021-05-19 16:20:55 -0400

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,31 +1,29 @@
-def myMax(list1: List[number]):
+lights = []
+degrees2 = 0
+light_level = 0
+degrees = 0
+greater1 = []
+lights2: List[number] = []
+def myMax(list1):
     max1 = list1[0]
     for x in list1:
         if x > max1:
             max1 = x
     return max1
-
-light_level = 0
-# unknown expression:  178
-lights = []
-lights2 = []
-greater1 = []
-degrees = 0
-degrees2 = 0
 while True:
     time = input.running_time_micros()
     if time == 1000:
         time = time - 1000
-        print('now')
-        for times in range(271):
+        print("now")
+        for index in range(270):
             degrees = degrees + 1
             light_level = input.light_level()
             pins.servo_write_pin(AnalogPin.P0, degrees)
-            lights[light_level] = degrees
-            lights.push(light_level)
+            lights.append(light_level)
+            lights[light_level] = [degrees]
             print(degrees)
         for key in lights:
             greater1.append(key)
         thelight = myMax(greater1)
-        degrees2 = lights[thelight]
+        degrees2 = lights.index(thelight)
         pins.servo_write_pin(AnalogPin.P0, degrees2)
--- main.ts
+++ main.ts
@@ -1,6 +1,12 @@
 let time: number;
-let thelight: number;
-function myMax(list1: number[]): number {
+let thelight: ?25533;
+let lights = []
+let degrees2 = 0
+let light_level = 0
+let degrees = 0
+let greater1 = []
+let lights2 : number[] = []
+function myMax(list1: any) {
     let max1 = list1[0]
     for (let x of list1) {
         if (x > max1) {
@@ -11,31 +17,24 @@ function myMax(list1: number[]): number {
     return max1
 }
 
-let light_level = 0
-//  unknown expression:  178
-let lights = []
-let lights2 = []
-let greater1 = []
-let degrees = 0
-let degrees2 = 0
 while (true) {
     time = input.runningTimeMicros()
     if (time == 1000) {
         time = time - 1000
         console.log("now")
-        for (let times = 0; times < 271; times++) {
+        for (let index = 0; index < 270; index++) {
             degrees = degrees + 1
             light_level = input.lightLevel()
             pins.servoWritePin(AnalogPin.P0, degrees)
-            lights[light_level] = degrees
             lights.push(light_level)
+            lights[light_level] = [degrees]
             console.log(degrees)
         }
         for (let key of lights) {
             greater1.push(key)
         }
         thelight = myMax(greater1)
-        degrees2 = lights[thelight]
+        degrees2 = _py.py_array_index(lights, thelight)
         pins.servoWritePin(AnalogPin.P0, degrees2)
     }
     
